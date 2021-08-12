0b3f8d6b2005de261cd87903a986eee096969f82
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-12 15:26:48 +0200

    sql sources update

--- src/main/java/com/example/dbinit/repository/PersonRepository.java
+++ src/main/java/com/example/dbinit/repository/PersonRepository.java
@@ -2,6 +2,8 @@ package com.example.dbinit.repository;
 
 import com.example.dbinit.model.Person;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends CrudRepository<Person, Long> {
 }
