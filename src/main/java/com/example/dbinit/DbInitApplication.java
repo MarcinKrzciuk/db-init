d70c4e89928b590f6c430b0bd7875ea990542346
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-12 15:26:43 +0200

    sql sources update

--- src/main/java/com/example/dbinit/DbInitApplication.java
+++ src/main/java/com/example/dbinit/DbInitApplication.java
@@ -1,6 +1,8 @@
 package com.example.dbinit;
 
+import com.example.dbinit.model.Address;
 import com.example.dbinit.model.Person;
+import com.example.dbinit.repository.AddressRepository;
 import com.example.dbinit.repository.PersonRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,11 +16,14 @@ public class DbInitApplication {
 		ConfigurableApplicationContext configurableApplicationContext =
 				SpringApplication.run(DbInitApplication.class, args);
 
+
 		PersonRepository personRepository = configurableApplicationContext.getBean(PersonRepository.class);
-
 		Person person = new Person("Jan", "Kowalski", 55);
-
 		personRepository.save(person);
-	}
 
+		AddressRepository addressRepository =configurableApplicationContext.getBean(AddressRepository.class);
+		Address address = new Address("Rolnicza", 7, 98765);
+		addressRepository.save(address);
+
+	}
 }
