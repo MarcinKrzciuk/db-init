f296ade4002b9bda633d60ebd00aaeb3ed9f4a18
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-12 11:42:48 +0200

    sql sources update

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO PERSON (first_name, last_name, age) VALUES ('John', 'Doe', 20);
 INSERT INTO PERSON (first_name, last_name, age) VALUES ('Ann', 'Nowak', 32);
 INSERT INTO PERSON (first_name, last_name, age) VALUES ('Jane', 'Smith', 40);
+
+INSERT INTO ADDRESS (street, street_nr, zip_code) VALUES ('Ladna', 16, 4100);
+INSERT INTO ADDRESS (street, street_nr, zip_code) VALUES ('Mocna', 145, 82060);
+INSERT INTO ADDRESS (street, street_nr, zip_code) VALUES ('Kolorowa', 90, 74199);
