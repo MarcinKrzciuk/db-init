d35eb15518d25ecd2138bbe482a11938d43ae22f
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-13 12:32:42 +0200

    sql sources update

---
+++ src/main/java/com/example/dbinit/entity/Person.java
@@ -0,0 +1,30 @@
+package com.example.dbinit.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_person")
+    private Long idPerson;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private Integer age;
+    @Column(name = "id_address")
+    private Long idAddress;
+
+    public Person(String firstName, String lastName, Integer age, Long idAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.idAddress = idAddress;
+    }
+}
