e0ce39506228d139893e6be485a3f3eecacadb7e
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-13 12:32:00 +0200

    sql sources update

---
+++ src/main/java/com/example/dbinit/entity/Address.java
@@ -0,0 +1,30 @@
+package com.example.dbinit.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_address")
+    private Long idAddress;
+    private String street;
+    @Column(name = "street_nr")
+    private Integer streetNr;
+    @Column(name = "zip_code")
+    private Integer zipCode;
+
+    public Address(String street, Integer streetNr, Integer zipCode) {
+        this.street = street;
+        this.streetNr = streetNr;
+        this.zipCode = zipCode;
+    }
+}
+
