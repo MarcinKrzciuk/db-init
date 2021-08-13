f1c395daf166c01302338e5c20b25178880142c7
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-13 07:30:27 +0200

    sql sources update

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO PERSON (first_name, last_name, age) VALUES ('John', 'Doe', 20);
-INSERT INTO PERSON (first_name, last_name, age) VALUES ('Ann', 'Nowak', 32);
-INSERT INTO PERSON (first_name, last_name, age) VALUES ('Jane', 'Smith', 40);
+INSERT INTO PERSON (first_name, last_name, age, id_address) VALUES ('John', 'Doe', 20, 1);
+INSERT INTO PERSON (first_name, last_name, age, id_address) VALUES ('Ann', 'Nowak', 32, 1);
+INSERT INTO PERSON (first_name, last_name, age, id_address) VALUES ('Jane', 'Smith', 40, 1);
 
 INSERT INTO ADDRESS (street, street_nr, zip_code) VALUES ('Ladna', 16, 4100);
 INSERT INTO ADDRESS (street, street_nr, zip_code) VALUES ('Mocna', 145, 82060);
