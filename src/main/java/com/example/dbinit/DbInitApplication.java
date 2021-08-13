61c773fefe936266a689dfb6e62c4e461f73865c
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-13 12:32:12 +0200

    sql sources update

--- src/main/java/com/example/dbinit/DbInitApplication.java
+++ src/main/java/com/example/dbinit/DbInitApplication.java
@@ -1,12 +1,16 @@
 package com.example.dbinit;
 
-import com.example.dbinit.model.Address;
-import com.example.dbinit.model.Person;
+import com.example.dbinit.entity.Address;
+import com.example.dbinit.entity.Person;
 import com.example.dbinit.repository.AddressRepository;
 import com.example.dbinit.repository.PersonRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @SpringBootApplication
 public class DbInitApplication {
@@ -17,13 +21,16 @@ public class DbInitApplication {
 				SpringApplication.run(DbInitApplication.class, args);
 
 
-//		PersonRepository personRepository = configurableApplicationContext.getBean(PersonRepository.class);
-//		Person person = new Person("Jan", "Kowalski", 55);
-//		personRepository.save(person);
-//
-//		AddressRepository addressRepository =configurableApplicationContext.getBean(AddressRepository.class);
-//		Address address = new Address("Rolnicza", 7, 98765);
-//		addressRepository.save(address);
+		AddressRepository addressRepository =configurableApplicationContext.getBean(AddressRepository.class);
+		Address address = new Address("Rolnicza", 7, 98765);
+		addressRepository.save(address);
+
+		PersonRepository personRepository = configurableApplicationContext.getBean(PersonRepository.class);
+		Person person = new Person("Jan", "Kowalski", 55, 1L);
+		personRepository.save(person);
+
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("test");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 
 	}
 }
--- src/main/java/com/example/dbinit/repository/AddressRepository.java
+++ src/main/java/com/example/dbinit/repository/AddressRepository.java
@@ -1,6 +1,6 @@
 package com.example.dbinit.repository;
 
-import com.example.dbinit.model.Address;
+import com.example.dbinit.entity.Address;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
