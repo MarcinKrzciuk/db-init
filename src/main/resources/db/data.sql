c01c9094345cb0db296a204f98794f45ab2ed304
Marcin Krzciuk <martin.krzciuk@gmail.com>
2021-08-14 11:06:27 +0200

    updated inserts for database

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO PERSON (first_name, last_name, age, id_address) VALUES ('John', 'Doe', 20, 1);
-INSERT INTO PERSON (first_name, last_name, age, id_address) VALUES ('Ann', 'Nowak', 32, 1);
-INSERT INTO PERSON (first_name, last_name, age, id_address) VALUES ('Jane', 'Smith', 40, 1);
+INSERT INTO PERSON (first_name, last_name, age, id_address, gender) VALUES ('John', 'Doe', 20, 1, DEFAULT);
+INSERT INTO PERSON (first_name, last_name, age, id_address, gender) VALUES ('Ann', 'Nowak', 32, 1, DEFAULT);
+INSERT INTO PERSON (first_name, last_name, age, id_address, gender) VALUES ('Jane', 'Smith', 40, 1, DEFAULT);
 
 INSERT INTO ADDRESS (street, street_nr, zip_code) VALUES ('Ladna', 16, 4100);
 INSERT INTO ADDRESS (street, street_nr, zip_code) VALUES ('Mocna', 145, 82060);
